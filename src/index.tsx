0f453fd4c956ff486f31463212354b7b642b3d01
JulesGilli <jules.gilli@epitech.eu>
2025-11-14 00:21:53 +0100

    refactor: switch routing from BrowserRouter to HashRouter to remove .htaccess dependency

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { AchievementsProvider } from './context/AchievementsContext';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <BrowserRouter basename={import.meta.env.BASE_URL}> 
-        <AchievementsProvider>
-          <App />
-        </AchievementsProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
+    createRoot(rootElement).render(
+        <React.StrictMode>
+            <HashRouter>
+                <AchievementsProvider>
+                    <App />
+                </AchievementsProvider>
+            </HashRouter>
+        </React.StrictMode>
+    );
 }
